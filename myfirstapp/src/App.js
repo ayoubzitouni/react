b42960208f01ebfcb60ff88d57e3a3723d5cf5a1
ayoubzitouni <ayoubzitouni315@gmail.com>
2022-10-04 20:02:06 +0100

    final copy

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,27 +1,67 @@
-import {Button,Form} from 'react-bootstrap'
-
-
-
-
-
-
 import logo from './logo.svg';
-import './App.css';
+import {Navbar,Form, Button, Container}  from 'react-bootstrap'
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <form>
-          <Form.Label>First Name</Form.Label>
-          <Form.Control type='text' placeholder='Your Name'></Form.Control>
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control type='text'placeholder='Last Name'></Form.Control>
-          <Form.Label>Date of Birth</Form.Label>
-          <Form.Control type=''></Form.Control>
-        </form>
-        
-      </header>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand>
+            Form checkpoint
+          </Navbar.Brand>
+          <Navbar.Brand>
+            <Form.Check type='switch'></Form.Check>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container className='col-6'>
+      <Form className='mt-5'>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>First Name</Form.Label>
+        <Form.Control type="text" placeholder='Your name here'></Form.Control>
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control type="text" placeholder='Your Last name here'></Form.Control>
+      </Form.Group>
+      <Form.Group className='mt-3'>
+        <Form.Label>Date of birth</Form.Label>
+        <Form.Control type="Date"></Form.Control>
+      </Form.Group>
+      <Form.Group className='mt-3'>
+        <Form.Label>Password</Form.Label>
+        <Form.Control type='Password' placeholder='Write a password'></Form.Control>
+        <Form.Label>Re-write Password</Form.Label>
+        <Form.Control type='Password' placeholder='Re-write a password'></Form.Control>
+      </Form.Group>
+      <Form.Group className='mt-3'>
+        <Form.Label>Gender</Form.Label>
+          <Form.Group>
+          <div key={"inline-radio"}>
+            <Form.Check type='radio' name='gender' label="Man"></Form.Check>
+            <Form.Check type='radio' name='gender' label="Woman"></Form.Check>
+          </div>
+          </Form.Group>
+      </Form.Group>
+      <Form.Group className='mt-3'>
+        <Form.Check name="validation" label="Did you accept our terms"></Form.Check>
+      </Form.Group>
+      <fieldset>
+      <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+        <Button className='mt-3 mb-5'>Sign In</Button>
+      </a>
+      </fieldset>
+      </Form>
+      </Container>
     </div>
   );
 }
